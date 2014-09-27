77fbabd0dc44106b47fc3359d399c50136e550ee
Max Li <maxli@maxli.ca>
2014-09-27 16:24:15 -0700

    Initial commit

---
+++ Strava_Real_Pace.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Strava Real Pace
+// @namespace   http://www.maxli.org
+// @description Replaces moving pace with real pace on Strava
+// @include     http://www.strava.com/dashboard
+// @version     1
+// @grant       none
+// ==/UserScript==
+
